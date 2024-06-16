da9373a40f69c036b83112623938ad63780f26dd
tomohiro.yamaoka <overusevery@gmail.com>
2024-06-16 16:05:37 +0900

    update test data

--- src/handler/customerHandler_test.go
+++ src/handler/customerHandler_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/overusevery/golang-echo-practice2/src/domain/entity"
@@ -19,10 +20,19 @@ func TestCustomerHandler_GetCustomer(t *testing.T) {
 	ctrl := gomock.NewController(t) //t *testing.T)
 	defer ctrl.Finish()
 	m := mock_repository.NewMockCustomerRepository(ctrl)
-	m.EXPECT().GetCustomer(gomock.Eq(12)).Return(entity.Customer{ID: 12})
+	m.EXPECT().GetCustomer(gomock.Eq(1)).Return(entity.Customer{
+		ID:            1,
+		Name:          "山田 太郎",
+		Address:       "東京都練馬区豊玉北2-13-1",
+		ZIP:           "176-0013",
+		Phone:         "03-1234-5678",
+		MarketSegment: "個人",
+		Nation:        "日本",
+		Birthdate:     time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
+	})
 	h := NewCustomrHandler(*usecase.NewGetCustomerUseCase(m))
 	h.RegisterRouter(e)
-	req := httptest.NewRequest(http.MethodGet, "/customer/12", nil)
+	req := httptest.NewRequest(http.MethodGet, "/customer/1", nil)
 	res := httptest.NewRecorder()
 	e.ServeHTTP(res, req)
 	expectedJson, err := os.ReadFile("../../fixture/customer.json")
