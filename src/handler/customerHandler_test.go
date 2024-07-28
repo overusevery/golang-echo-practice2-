6904186e769f1ec6572e3a43df6342706feb8044
tomohiro.yamaoka <overusevery@gmail.com>
2024-07-28 23:00:11 +0900

    refactor test code

--- src/handler/customerHandler_test.go
+++ src/handler/customerHandler_test.go
@@ -19,62 +19,109 @@ import (
 )
 
 func TestCustomerHandler_GetCustomer(t *testing.T) {
-	e := echo.New()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	m := mock_repository.NewMockCustomerRepository(ctrl)
-	m.EXPECT().GetCustomer(context.Background(), gomock.Eq(1)).Return(&entity.Customer{
-		ID:            1,
-		Name:          "山田 太郎",
-		Address:       "東京都練馬区豊玉北2-13-1",
-		ZIP:           "176-0013",
-		Phone:         "03-1234-5678",
-		MarketSegment: "個人",
-		Nation:        "日本",
-		Birthdate:     time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
-	}, nil)
-	h := NewGetCustomrHandler(customerusecase.NewGetCustomerUseCase(m))
-	h.RegisterRouter(e)
+	setupGetCustomerHandlerWithMock(t, func(m *mock_repository.MockCustomerRepository, e *echo.Echo) {
+		m.EXPECT().GetCustomer(context.Background(), gomock.Eq(1)).Return(&entity.Customer{
+			ID:            1,
+			Name:          "山田 太郎",
+			Address:       "東京都練馬区豊玉北2-13-1",
+			ZIP:           "176-0013",
+			Phone:         "03-1234-5678",
+			MarketSegment: "個人",
+			Nation:        "日本",
+			Birthdate:     time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
+		}, nil)
 
-	res := testutil.GET(e, "/customer/1")
+		res := testutil.GET(e, "/customer/1")
 
-	expectedJson, err := os.ReadFile("../../fixture/get_customer_response.json")
-	if err != nil {
-		panic(err)
-	}
+		expectedJson, err := os.ReadFile("../../fixture/get_customer_response.json")
+		if err != nil {
+			panic(err)
+		}
 
-	assert.JSONEq(t, string(expectedJson), res.Body.String())
+		assert.JSONEq(t, string(expectedJson), res.Body.String())
+	})
 }
 
 func TestCustomerHandler_GetCustomer_NotFound(t *testing.T) {
-	e := echo.New()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	m := mock_repository.NewMockCustomerRepository(ctrl)
-	m.EXPECT().GetCustomer(context.Background(), gomock.Eq(1)).Return(nil, repository.ErrCustomerNotFound)
-	h := NewGetCustomrHandler(customerusecase.NewGetCustomerUseCase(m))
-	h.RegisterRouter(e)
+	setupGetCustomerHandlerWithMock(t, func(m *mock_repository.MockCustomerRepository, e *echo.Echo) {
+		m.EXPECT().GetCustomer(context.Background(), gomock.Eq(1)).Return(nil, repository.ErrCustomerNotFound)
 
-	res := testutil.GET(e, "/customer/1")
+		res := testutil.GET(e, "/customer/1")
 
-	assert.Equal(t, http.StatusNotFound, res.Result().StatusCode)
+		assert.Equal(t, http.StatusNotFound, res.Result().StatusCode)
+	})
 }
 
 func TestCustomerHandler_When_Unexpected_Error_Happened_InternalServerError_Should_be_Returned(t *testing.T) {
-	e := echo.New()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	m := mock_repository.NewMockCustomerRepository(ctrl)
-	m.EXPECT().GetCustomer(gomock.Any(), gomock.Any()).Return(nil, errors.New("some error"))
-	h := NewGetCustomrHandler(customerusecase.NewGetCustomerUseCase(m))
-	h.RegisterRouter(e)
+	setupGetCustomerHandlerWithMock(t, func(m *mock_repository.MockCustomerRepository, e *echo.Echo) {
+		m.EXPECT().GetCustomer(gomock.Any(), gomock.Any()).Return(nil, errors.New("some error"))
 
-	res := testutil.GET(e, "/customer/1")
+		res := testutil.GET(e, "/customer/1")
 
-	assert.Equal(t, http.StatusInternalServerError, res.Result().StatusCode)
+		assert.Equal(t, http.StatusInternalServerError, res.Result().StatusCode)
+	})
 }
 
 func TestCustomerHandler_Invalid_Query_Should_Return_Bad_Request(t *testing.T) {
+	setupGetCustomerHandlerWithMock(t, func(m *mock_repository.MockCustomerRepository, e *echo.Echo) {
+
+		res := testutil.GET(e, "/customer/not_number")
+
+		assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
+	})
+}
+
+func TestCustomerHandler_CreateCustomer(t *testing.T) {
+	setupCreateCustomerHandlerWithMock(t,
+		func(m *mock_repository.MockCustomerRepository, e *echo.Echo) {
+			m.EXPECT().CreateCustomer(context.Background(), gomock.Eq(entity.Customer{
+				ID:            1,
+				Name:          "山田 太郎",
+				Address:       "東京都練馬区豊玉北2-13-1",
+				ZIP:           "176-0013",
+				Phone:         "03-1234-5678",
+				MarketSegment: "個人",
+				Nation:        "日本",
+				Birthdate:     time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
+			})).Return(&entity.Customer{
+				ID:            1,
+				Name:          "山田 太郎",
+				Address:       "東京都練馬区豊玉北2-13-1",
+				ZIP:           "176-0013",
+				Phone:         "03-1234-5678",
+				MarketSegment: "個人",
+				Nation:        "日本",
+				Birthdate:     time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
+			}, nil)
+
+			res := testutil.Post(e, "/customer", "../../fixture/create_customer_request.json")
+
+			expectedJson, err := os.ReadFile("../../fixture/create_customer_response.json")
+			if err != nil {
+				panic(err)
+			}
+			assert.JSONEq(t, string(expectedJson), res.Body.String())
+		})
+}
+
+func TestCustomerHandler_CreateCustomer_Bad_Request(t *testing.T) {
+	setupCreateCustomerHandlerWithMock(t,
+		func(m *mock_repository.MockCustomerRepository, e *echo.Echo) {
+			res := testutil.Post(e, "/customer", "../../fixture/create_customer_request_invalid.json")
+			assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
+		})
+}
+
+func TestCustomerHandler_CreateCustomer_When_Unexpected_Error_Happened_InternalServerError_Should_be_Returned(t *testing.T) {
+	setupCreateCustomerHandlerWithMock(t,
+		func(m *mock_repository.MockCustomerRepository, e *echo.Echo) {
+			m.EXPECT().CreateCustomer(gomock.Any(), gomock.Any()).Return(nil, errors.New("some error"))
+			res := testutil.Post(e, "/customer", "../../fixture/create_customer_request.json")
+			assert.Equal(t, http.StatusInternalServerError, res.Result().StatusCode)
+		})
+}
+
+func setupGetCustomerHandlerWithMock(t *testing.T, testFun func(m *mock_repository.MockCustomerRepository, e *echo.Echo)) {
 	e := echo.New()
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -82,65 +129,16 @@ func TestCustomerHandler_Invalid_Query_Should_Return_Bad_Request(t *testing.T) {
 	h := NewGetCustomrHandler(customerusecase.NewGetCustomerUseCase(m))
 	h.RegisterRouter(e)
 
-	res := testutil.GET(e, "/customer/not_number")
-
-	assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
+	testFun(m, e)
 }
 
-func TestCustomerHandler_CreateCustomer(t *testing.T) {
-	e := echo.New()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	m := mock_repository.NewMockCustomerRepository(ctrl)
-	m.EXPECT().CreateCustomer(context.Background(), gomock.Eq(entity.Customer{
-		ID:            1,
-		Name:          "山田 太郎",
-		Address:       "東京都練馬区豊玉北2-13-1",
-		ZIP:           "176-0013",
-		Phone:         "03-1234-5678",
-		MarketSegment: "個人",
-		Nation:        "日本",
-		Birthdate:     time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
-	})).Return(&entity.Customer{
-		ID:            1,
-		Name:          "山田 太郎",
-		Address:       "東京都練馬区豊玉北2-13-1",
-		ZIP:           "176-0013",
-		Phone:         "03-1234-5678",
-		MarketSegment: "個人",
-		Nation:        "日本",
-		Birthdate:     time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
-	}, nil)
-	h := NewCreateCustomerHandler(customerusecase.NewCreateCustomerUseCase(m))
-	h.RegisterRouter(e)
-	res := testutil.Post(e, "/customer", "../../fixture/create_customer_request.json")
-	expectedJson, err := os.ReadFile("../../fixture/create_customer_response.json")
-	if err != nil {
-		panic(err)
-	}
-
-	assert.JSONEq(t, string(expectedJson), res.Body.String())
-}
-
-func TestCustomerHandler_CreateCustomer_Bad_Request(t *testing.T) {
+func setupCreateCustomerHandlerWithMock(t *testing.T, testFun func(m *mock_repository.MockCustomerRepository, e *echo.Echo)) {
 	e := echo.New()
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	m := mock_repository.NewMockCustomerRepository(ctrl)
 	h := NewCreateCustomerHandler(customerusecase.NewCreateCustomerUseCase(m))
 	h.RegisterRouter(e)
-	res := testutil.Post(e, "/customer", "../../fixture/create_customer_request_invalid.json")
-	assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
-}
 
-func TestCustomerHandler_CreateCustomer_When_Unexpected_Error_Happened_InternalServerError_Should_be_Returned(t *testing.T) {
-	e := echo.New()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	m := mock_repository.NewMockCustomerRepository(ctrl)
-	m.EXPECT().CreateCustomer(gomock.Any(), gomock.Any()).Return(nil, errors.New("some error"))
-	h := NewCreateCustomerHandler(customerusecase.NewCreateCustomerUseCase(m))
-	h.RegisterRouter(e)
-	res := testutil.Post(e, "/customer", "../../fixture/create_customer_request.json")
-	assert.Equal(t, http.StatusInternalServerError, res.Result().StatusCode)
+	testFun(m, e)
 }
