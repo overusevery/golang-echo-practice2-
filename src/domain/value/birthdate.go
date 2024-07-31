231c0423d523ecd5be085df0142f1833bff4321f
tomohiro.yamaoka <overusevery@gmail.com>
2024-07-31 15:11:43 +0900

    add Birthdate value obj validateion

--- src/domain/value/birthdate.go
+++ src/domain/value/birthdate.go
@@ -9,20 +9,24 @@ type Birthdate time.Time
 
 var (
 	ErrTooOldDate = errors.New("Birthdate is too old")
+	ErrFutureDate = errors.New("Birthdate cannot be future")
 )
 
-func NewBirthdate(t time.Time) (Birthdate, error) {
+func NewBirthdate(t time.Time, now time.Time) (Birthdate, error) {
 	b := Birthdate(t)
-	validateionErrors := b.validate()
+	validateionErrors := b.validate(now)
 	if validateionErrors != nil {
 		return Birthdate{}, validateionErrors
 	}
 	return b, nil
 }
 
-func (b *Birthdate) validate() error {
+func (b *Birthdate) validate(now time.Time) error {
 	var validateionErrors error
 	if time.Time(*b).Before(time.Date(1800, 1, 1, 0, 0, 0, 0, time.Local)) {
+		validateionErrors = errors.Join(validateionErrors, ErrTooOldDate)
+	}
+	if time.Time(*b).After(now) {
 		validateionErrors = errors.Join(validateionErrors, ErrTooOldDate)
 	}
 	return validateionErrors
--- src/domain/value/birthdate_test.go
+++ src/domain/value/birthdate_test.go
@@ -7,8 +7,10 @@ import (
 )
 
 func TestNewBirthdate(t *testing.T) {
+	NOW := time.Now()
 	type args struct {
 		in0 time.Time
+		now time.Time
 	}
 	tests := []struct {
 		name    string
@@ -18,20 +20,50 @@ func TestNewBirthdate(t *testing.T) {
 	}{
 		{
 			name:    "success",
-			args:    args{in0: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local)},
+			args:    args{in0: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local), now: NOW},
 			want:    Birthdate(time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local)),
 			wantErr: false,
 		},
 		{
-			name:    "too old",
-			args:    args{in0: time.Date(20, 1, 1, 0, 0, 0, 0, time.Local)},
+			name:    "Birthdate Should not be too old(<1800/1/1 is invalid)",
+			args:    args{in0: time.Date(1799, 1, 1, 0, 0, 0, 0, time.Local), now: NOW},
+			want:    Birthdate{},
+			wantErr: true,
+		},
+		{
+			name:    "Birthdate Should not be too old(1800/1/1 is valid)",
+			args:    args{in0: time.Date(1800, 1, 1, 0, 0, 0, 0, time.Local), now: NOW},
+			want:    Birthdate(time.Date(1800, 1, 1, 0, 0, 0, 0, time.Local)),
+			wantErr: false,
+		},
+		{
+			name:    "Birthdate Should not be too old(>1800/1/1 is valid)",
+			args:    args{in0: time.Date(1800, 1, 2, 0, 0, 0, 0, time.Local), now: NOW},
+			want:    Birthdate(time.Date(1800, 1, 2, 0, 0, 0, 0, time.Local)),
+			wantErr: false,
+		},
+		{
+			name:    "Birthdate Should not be future(now-1milisec is valid)",
+			args:    args{in0: NOW.Add(time.Microsecond * -1), now: NOW},
+			want:    Birthdate(NOW.Add(time.Microsecond * -1)),
+			wantErr: false,
+		},
+		{
+			name:    "Birthdate Should not be future(now is valid)",
+			args:    args{in0: NOW, now: NOW},
+			want:    Birthdate(NOW),
+			wantErr: false,
+		},
+		{
+			name:    "Birthdate Should not be future(now+1milisec is invalid)",
+			args:    args{in0: NOW.Add(time.Microsecond), now: NOW},
 			want:    Birthdate{},
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewBirthdate(tt.args.in0)
+			got, err := NewBirthdate(tt.args.in0, tt.args.now)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewBirthdate() error = %v, wantErr %v", err, tt.wantErr)
 				return
