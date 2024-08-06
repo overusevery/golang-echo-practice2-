713c893666d5a073d8f892be0499b44019704536
tomohiro.yamaoka <overusevery@gmail.com>
2024-08-06 11:31:11 +0900

    refactor:response format of get customer

--- src/handler/customerhandler/getCustomerHandler.go
+++ src/handler/customerhandler/getCustomerHandler.go
@@ -9,6 +9,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/overusevery/golang-echo-practice2/src/domain/repository"
 	"github.com/overusevery/golang-echo-practice2/src/domain/usecase/customerusecase"
+	"golang.org/x/exp/slog"
 )
 
 type CustomerHandler struct {
@@ -26,16 +27,21 @@ func (h *CustomerHandler) RegisterRouter(e *echo.Echo) {
 func (h *CustomerHandler) GetCustomer(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest)
+		return c.JSON(http.StatusBadRequest, convertToGetCustomerMultiResponse(err))
 	}
 
 	customer, err := h.GetCustomerUseCase.Execute(c.Request().Context(), id)
 	if err != nil {
 		switch {
 		case errors.Is(err, repository.ErrCustomerNotFound):
-			return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Customer (id = %v) is not found", id))
+			return c.JSON(http.StatusNotFound, GetCustomerErrorResponse{
+				Message: fmt.Sprintf("Customer (id = %v) is not found", id),
+			})
 		default:
-			return echo.NewHTTPError(http.StatusInternalServerError)
+			slog.Error("GetCustomer get unexpected error", "detail", err)
+			return c.JSON(http.StatusInternalServerError, CreateCustomerErrorResponse{
+				Message: "failed to get customer",
+			})
 		}
 	}
 
--- src/handler/customerhandler/getCustomerResponse.go
+++ src/handler/customerhandler/getCustomerResponse.go
@@ -1,13 +1,18 @@
 package customerhandler
 
 import (
+	"errors"
 	"time"
 
 	"github.com/overusevery/golang-echo-practice2/src/domain/entity"
 	openapi "github.com/overusevery/golang-echo-practice2/src/handler/openapigenmodel/go"
+	"github.com/overusevery/golang-echo-practice2/src/shared/message"
+	"github.com/overusevery/golang-echo-practice2/src/shared/util"
 )
 
 type GetCustomerResponse openapi.Customer
+type GetCustomerErrorResponse openapi.Error
+type GetCustomerMultiErrorResponse openapi.MultipleErrorResponse
 
 func convertFrom(customer entity.Customer) GetCustomerResponse {
 	return GetCustomerResponse{
@@ -19,5 +24,33 @@ func convertFrom(customer entity.Customer) GetCustomerResponse {
 		Mktsegment: customer.MarketSegment,
 		Nation:     string(customer.Nation),
 		Birthdate:  time.Time(customer.Birthdate),
+	}
+}
+
+func convertToGetCustomerMultiResponse(err error) GetCustomerMultiErrorResponse {
+	messages := []openapi.ErrorElement{}
+	var errList *util.ValidationErrorList
+	if errors.As(err, &errList) {
+		for _, err := range errList.Unwrap() {
+			if customErr, ok := err.(*message.ErrorWithId); ok {
+				messages = append(messages,
+					openapi.ErrorElement{
+						Id:  customErr.ErrorID(),
+						Msg: customErr.Error(),
+					},
+				)
+			} else {
+				messages = append(messages,
+					openapi.ErrorElement{
+						Id:  message.ERRID00004.ErrorID(),
+						Msg: errors.Join(message.ERRID00004, err).Error(),
+					},
+				)
+			}
+		}
+	}
+
+	return GetCustomerMultiErrorResponse{
+		Errors: messages,
 	}
 }
