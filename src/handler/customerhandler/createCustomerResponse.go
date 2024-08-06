bfd979d09ac472070f5e9e443405af6bfeb81ea4
tomohiro.yamaoka <overusevery@gmail.com>
2024-08-06 11:35:57 +0900

    refactore:merge create customer response files

--- src/handler/customerhandler/createCustomerResponse.go
+++ src/handler/customerhandler/createCustomerResponse.go
@@ -1,13 +1,46 @@
 package customerhandler
 
 import (
+	"errors"
 	"time"
 
 	"github.com/overusevery/golang-echo-practice2/src/domain/entity"
 	openapi "github.com/overusevery/golang-echo-practice2/src/handler/openapigenmodel/go"
+	"github.com/overusevery/golang-echo-practice2/src/shared/message"
+	"github.com/overusevery/golang-echo-practice2/src/shared/util"
 )
 
 type CreateCustomerResponse openapi.Customer
+type CreateCustomerErrorResponse openapi.Error
+type CreateCustomerMultiErrorResponse openapi.MultipleErrorResponse
+
+func convertToCreateCustomerErrorResponse(err error) CreateCustomerMultiErrorResponse {
+	messages := []openapi.ErrorElement{}
+	var errList *util.ValidationErrorList
+	if errors.As(err, &errList) {
+		for _, err := range errList.Unwrap() {
+			if customErr, ok := err.(*message.ErrorWithId); ok {
+				messages = append(messages,
+					openapi.ErrorElement{
+						Id:  customErr.ErrorID(),
+						Msg: customErr.Error(),
+					},
+				)
+			} else {
+				messages = append(messages,
+					openapi.ErrorElement{
+						Id:  message.ERRID00004.ErrorID(),
+						Msg: errors.Join(message.ERRID00004, err).Error(),
+					},
+				)
+			}
+		}
+	}
+
+	return CreateCustomerMultiErrorResponse{
+		Errors: messages,
+	}
+}
 
 func convertToCreateCustomerResponse(customer entity.Customer) CreateCustomerResponse {
 	return CreateCustomerResponse{
