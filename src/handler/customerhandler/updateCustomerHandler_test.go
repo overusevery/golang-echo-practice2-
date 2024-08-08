75bb1fbc77883fccf4b0a6c1962590a1900e8720
tomohiro.yamaoka <overusevery@gmail.com>
2024-08-08 15:01:35 +0900

    temp:extract mock helper

--- src/handler/customerhandler/updateCustomerHandler_test.go
+++ src/handler/customerhandler/updateCustomerHandler_test.go
@@ -14,27 +14,31 @@ import (
 
 func TestUpdateCustomerHandler(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		e := echo.New()
-
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		m := mock_repository.NewMockCustomerRepository(ctrl)
-
-		h := &UpdateCustomerHandler{
-			UpdateCustomerUseCase: &customerusecase.UpdateCustomerUseCase{Repository: m},
-		}
-		h.RegisterRouter(e)
+		e, m, close := setUpdateCustomerMock(t)
+		defer close()
 		m.EXPECT().UpdateCustomer(gomock.Any(), gomock.Any())
 		res := testutil.PUT(e, "/customer/1", "../../../fixture/put_customer_request.json")
 		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
 	})
 	t.Run("fake:internal server error", func(t *testing.T) {
-		e := echo.New()
-		h := &UpdateCustomerHandler{
-			UpdateCustomerUseCase: &customerusecase.UpdateCustomerUseCase{},
-		}
-		h.RegisterRouter(e)
+		e, m, close := setUpdateCustomerMock(t)
+		defer close()
+		m.EXPECT().UpdateCustomer(gomock.Any(), gomock.Any())
 		res := testutil.PUT(e, "/customer/2", "../../../fixture/put_customer_request.json")
 		assert.Equal(t, http.StatusInternalServerError, res.Result().StatusCode)
 	})
+}
+
+func setUpdateCustomerMock(t *testing.T) (*echo.Echo, *mock_repository.MockCustomerRepository, func()) {
+	e := echo.New()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := mock_repository.NewMockCustomerRepository(ctrl)
+
+	h := &UpdateCustomerHandler{
+		UpdateCustomerUseCase: &customerusecase.UpdateCustomerUseCase{Repository: m},
+	}
+	h.RegisterRouter(e)
+	return e, m, ctrl.Finish
 }
