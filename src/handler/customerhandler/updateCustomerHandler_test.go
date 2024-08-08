323001cf5c730257593fd16539c531869a6f2104
tomohiro.yamaoka <overusevery@gmail.com>
2024-08-08 15:21:32 +0900

    temp:updaate customer usecase should be called with customer(red)

--- src/handler/customerhandler/updateCustomerHandler_test.go
+++ src/handler/customerhandler/updateCustomerHandler_test.go
@@ -3,6 +3,7 @@ package customerhandler
 import (
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/overusevery/golang-echo-practice2/src/domain/usecase/customerusecase"
@@ -16,7 +17,16 @@ func TestUpdateCustomerHandler(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		e, m, close := setUpdateCustomerMock(t)
 		defer close()
-		m.EXPECT().UpdateCustomer(gomock.Any(), gomock.Any())
+		m.EXPECT().UpdateCustomer(gomock.Any(), gomock.Eq(forceNewCustomer(
+			"1",
+			"山田 太郎",
+			"東京都練馬区豊玉北2-13-1",
+			"176-0013",
+			"03-1234-5678",
+			"個人",
+			"日本",
+			time.Date(1980, 1, 1, 0, 0, 0, 0, time.UTC),
+		)))
 		res := testutil.PUT(e, "/customer/1", "../../../fixture/put_customer_request.json")
 		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
 	})
