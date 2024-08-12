c4a1ef5bec58150e1eb6c24bb3c9722a4e869589
tomohiro.yamaoka <overusevery@gmail.com>
2024-08-12 09:21:19 +0900

    extract setupMock

--- src/handler/customerhandler/deleteCustomerHandler_test.go
+++ src/handler/customerhandler/deleteCustomerHandler_test.go
@@ -14,15 +14,23 @@ import (
 
 func TestDeleteCustomerHandler_DeleteCustomer(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		e := echo.New()
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		m := mock_repository.NewMockCustomerRepository(ctrl)
-		h := NewDeleteCustomerHandler(*customerusecase.NewDeleteCustomerUseCase(m))
-		h.RegisterRouter(e)
+		e, close, m := newFunction(t)
+		defer close()
 
 		m.EXPECT().DeleteCustomer(gomock.Any(), "1")
 		res := testutil.GET(e, "/customer/1")
 		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
 	})
+}
+
+func newFunction(t *testing.T) (*echo.Echo, func(), *mock_repository.MockCustomerRepository) {
+	e := echo.New()
+	ctrl := gomock.NewController(t)
+	close := func() {
+		ctrl.Finish()
+	}
+	m := mock_repository.NewMockCustomerRepository(ctrl)
+	h := NewDeleteCustomerHandler(*customerusecase.NewDeleteCustomerUseCase(m))
+	h.RegisterRouter(e)
+	return e, close, m
 }
